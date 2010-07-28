22c434b0f3af49272cb73b24077788eb16dfe8f3
Andrzej Giniewicz <gginiu@gmail.com>
2010-07-28 15:01:05 +0200

    fix for autogenerated part of documentation, for future, please use real name when working with repository

--- doc/generate.sh
+++ doc/generate.sh
@@ -7,6 +7,9 @@ git log -1 --pretty=format:"<pubsnumber>Rev: %h</pubsnumber>" >> revision.data
 # generate history data
 echo "<?xml version='1.0' encoding='UTF-8'?>" > history.data
 echo "<revhistory>" >> history.data
-git log --reverse --pretty=format:"<revision><revnumber>%h</revnumber><date>%ai</date><authorinitials>%cN</authorinitials><revdescription><para>%s</para></revdescription></revision>" >> history.data
+git log --reverse --pretty=format:"<revision><revnumber>%h</revnumber><date>%ai</date><authorinitials>%cN</authorinitials><revdescription><para>%s</para></revdescription></revision>" | \
+  sed "s/<authorinitials>bartbes<\/authorinitials>/<authorinitials>Bart van Strien<\/authorinitials>/g" | \
+  sed "s/<authorinitials>root<\/authorinitials>/<authorinitials>William Bowers<\/authorinitials>/g" \
+  >> history.data
 echo "</revhistory>" >> history.data
 
