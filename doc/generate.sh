b44ddad2ec84bce273cc1d766890e3f0036968e9
Andrzej Giniewicz <gginiu@gmail.com>
2011-09-09 18:17:48 +0200

    Added new contributor, fixed comment conversion to xml, fixed roles

--- doc/generate.sh
+++ doc/generate.sh
@@ -9,7 +9,7 @@ echo "<?xml version='1.0' encoding='UTF-8'?>" > history.data
 echo "<revhistory>" >> history.data
 git log --reverse --pretty=format:"<revision><revnumber>%h</revnumber><date>%ai</date><authorinitials>%cN</authorinitials><revdescription><para>%s</para></revdescription></revision>" | \
   sed "s/<authorinitials>bartbes<\/authorinitials>/<authorinitials>Bart van Strien<\/authorinitials>/g" | \
-  sed "s/<authorinitials>root<\/authorinitials>/<authorinitials>William Bowers<\/authorinitials>/g" \
-  >> history.data
+  sed "s/<authorinitials>root<\/authorinitials>/<authorinitials>William Bowers<\/authorinitials>/g" | \
+  sed "s/&/&amp;/g"  >> history.data
 echo "</revhistory>" >> history.data
 
